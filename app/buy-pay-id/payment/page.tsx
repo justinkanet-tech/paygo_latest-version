9cbb0f619311f2a5bfdcabdf21d0b842ca81db1b
justinkanet-tech <justinkanet@gmail.com>
2026-06-18 18:40:31 +0100

    Update page.tsx

--- app/buy-pay-id/payment/page.tsx
+++ app/buy-pay-id/payment/page.tsx
@@ -30,7 +30,7 @@ export default function PaymentPage() {
   }
 
   const handleCopyAccountNumber = () => {
-    navigator.clipboard.writeText("0438704497")
+    navigator.clipboard.writeText("7016809155")
     setCopiedAccount(true)
     setTimeout(() => setCopiedAccount(false), 2000)
   }
@@ -89,7 +89,7 @@ export default function PaymentPage() {
             <div>
               <p className="text-gray-700 mb-1">Account Number</p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">0438704497</p>
+                <p className="font-bold">9036502681</p>
                 <button
                   onClick={handleCopyAccountNumber}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
@@ -101,12 +101,12 @@ export default function PaymentPage() {
 
             <div>
               <p className="text-gray-700 mb-1">Bank Name</p>
-              <p className="font-bold">WEMA BANK</p>
+              <p className="font-bold">MOMO PAYMENT</p>
             </div>
 
             <div>
               <p className="text-gray-700 mb-1">Account Name</p>
-              <p className="font-bold">Wale Obamoyegun</p>
+              <p className="font-bold">OLORUNWA MAYOR</p>
             </div>
           </div>
 
