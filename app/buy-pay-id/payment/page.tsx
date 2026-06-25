9a06880e7ee2d2f2f6eabf7132399411b1a3e7ba
justinkanet-tech <justinkanet@gmail.com>
2026-06-25 18:47:21 +0100

    Update page.tsx

--- app/buy-pay-id/payment/page.tsx
+++ app/buy-pay-id/payment/page.tsx
@@ -89,7 +89,7 @@ export default function PaymentPage() {
             <div>
               <p className="text-gray-700 mb-1">Account Number</p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">9036502681</p>
+                <p className="font-bold">9162894596</p>
                 <button
                   onClick={handleCopyAccountNumber}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
@@ -101,12 +101,12 @@ export default function PaymentPage() {
 
             <div>
               <p className="text-gray-700 mb-1">Bank Name</p>
-              <p className="font-bold">MOMO PAYMENT</p>
+              <p className="font-bold">MONIEPOINT mfb</p>
             </div>
 
             <div>
               <p className="text-gray-700 mb-1">Account Name</p>
-              <p className="font-bold">OLORUNWA MAYOR</p>
+              <p className="font-bold">OLUKOYA JAPHET</p>
             </div>
           </div>
 
