1312b94ebf445ba4dab29439a369fcf43f359ded
justinkanet-tech <justinkanet@gmail.com>
2026-06-27 10:50:25 +0100

    Update page.tsx

--- app/upgrade/payment/page.tsx
+++ app/upgrade/payment/page.tsx
@@ -32,7 +32,7 @@ export default function UpgradePaymentPage() {
   }
 
   const handleCopyAccountNumber = () => {
-    navigator.clipboard.writeText("0438704497")
+    navigator.clipboard.writeText("9162894596")
     setCopiedAccount(true)
     setTimeout(() => setCopiedAccount(false), 2000)
   }
@@ -95,7 +95,7 @@ export default function UpgradePaymentPage() {
                 <span>🔢</span> Account Number
               </p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">*0438704497*</p>
+                <p className="font-bold">*9162894596*</p>
                 <button
                   onClick={handleCopyAccountNumber}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
@@ -109,14 +109,14 @@ export default function UpgradePaymentPage() {
               <p className="text-gray-700 mb-1 text-sm flex items-center gap-1">
                 <span>🏦</span> Bank Name
               </p>
-              <p className="font-bold">WEMA BANK</p>
+              <p className="font-bold">MONIPOINT</p>
             </div>
 
             <div>
               <p className="text-gray-700 mb-1 text-sm flex items-center gap-1">
                 <span>🚹</span> Account Name
               </p>
-              <p className="font-bold">WALE OBAMOYEGUN</p>
+              <p className="font-bold">OLUKOYA JAPHET</p>
             </div>
           </div>
 
