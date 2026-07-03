e1e1fad2f6492de515c67f1e4404935ca7498d90
justinkanet-tech <justinkanet@gmail.com>
2026-07-03 09:34:13 +0100

    Update page.tsx

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -27,7 +27,7 @@ export default function SupportPage() {
 
   const handleTelegramSupport = () => {
     // Open Telegram support link in a new tab
-    const telegramUrl = "https://t.me/vincent12655"
+    const telegramUrl = "https://t.me/Matthewxx8230"
     window.open(telegramUrl, "_blank")
   }
 
