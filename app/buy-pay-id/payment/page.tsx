943900a7f639c27be4b019eb373899ff5ba0aaaf
justinkanet-tech <justinkanet@gmail.com>
2026-09-15 14:56:05 +0100

    Update page.tsx

--- app/buy-pay-id/payment/page.tsx
+++ app/buy-pay-id/payment/page.tsx
@@ -76,7 +76,7 @@ export default function PaymentPage() {
             <div>
               <p className="text-gray-700 mb-1">Amount</p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">NGN 10,000</p>
+                <p className="font-bold">NGN 10,500</p>
                 <button
                   onClick={handleCopyAmount}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
